e7e4a1f08b5b11de9cb094a8736efa0bf900c999
Arush Chinchkhede <125636290+arushraju@users.noreply.github.com>
2025-12-23 16:30:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -30,3 +30,4 @@ app.post('/solve', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
